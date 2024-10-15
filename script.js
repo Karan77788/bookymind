1b7829256c79e8d4a31cf57791bf35f3237a54bb
Karan77788 <karanb.bcs22@rathinam.in>
2024-10-15 11:35:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var popupoverlay = document.querySelector(".popup-overlay")
+var popupbox = document.querySelector(".popup-box")
+var addpopupbutton = document.getElementById("add-popup")
+
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+
+var cancelpopup = document.getElementById("cancel-popup")
+
+cancelpopup.addEventListener("click",function(event){
+
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
